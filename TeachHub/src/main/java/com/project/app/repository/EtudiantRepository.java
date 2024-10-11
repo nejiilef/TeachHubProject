6428cf43a971ea71f33118844e991a538a1987d7
Ilef Neji <ilefneji334@gmail.com>
2024-10-11 18:52:11 +0100

    Ajout des fichiers de TeachHub et front/Teach

---
+++ TeachHub/src/main/java/com/project/app/repository/EtudiantRepository.java
@@ -0,0 +1,14 @@
+package com.project.app.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.app.models.Etudiant;
+
+@Repository
+public interface EtudiantRepository extends JpaRepository<Etudiant, Long>{
+	Optional<Etudiant> findByEmail(String email);
+	boolean existsByEmail(String email);
+}
---
+++ front/Teach/src/app/auth/login/login.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm!: FormGroup;
+  constructor(
+    private service: AuthService,
+    private fb: FormBuilder,
+    private router: Router
+  ) { }
+  
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      MotDePasse: ['', Validators.required],
+    })
+  }
+  
+  submitForm() {
+    this.service.login(this.loginForm.value).subscribe(
+      (response) => {
+        console.log(response);
+        if (response.jwt != null) {
+          localStorage.setItem("role",response.role);
+          console.log(response.role);
+          alert("Hello, Your token is " + response.jwt);
+          const jwtToken = response.jwt;
+          localStorage.setItem('jwt', jwtToken);
+          this.router.navigateByUrl("home");
+      
+      }},
+      (error)=>{
+        alert("Email ou mot de passe incorrect ! " );
+      }
+      )
+    
+  }
+}
